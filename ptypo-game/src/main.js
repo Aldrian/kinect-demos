d67535db0dcbca3dbb499df0c81e5ba01360b8c8
Aldrian <ibarretj@gmail.com>
2017-05-11 22:38:50 +0200

    prototypo-game modifications

--- ptypo-game/src/main.js
+++ ptypo-game/src/main.js
@@ -10,27 +10,27 @@ $(document).ready(function () {
         "interaction" : {
             "enabled": false,
         },
-     
+
         "userviewer" : {
             "enabled": false,
             //"resolution": "640x480", //320x240, 160x120, 128x96, 80x60
             //"userColors": { "engaged": 0x7fffffff, "tracked": 0x7fffffff },
             //"defaultUserColor": 0x70000000, //RGBA 2147483647
         },
-     
+
         "backgroundRemoval" : {
             "enabled": false,
             //"resolution": "640x480", //1280x960
         },
-     
+
         "skeleton" : {
             "enabled": true,
         },
-     
+
         "sensorStatus" : {
             "enabled": true,
         }
-     
+
     };
 
     // Create sensor and UI adapter layers
@@ -40,7 +40,7 @@ $(document).ready(function () {
     });
 
     sensor.addEventHandler(function (event) {
-                
+
         switch (event.category) {
             case Kinect.SENSORSTATUS_EVENT_CATEGORY:
                 switch (event.eventType) {
@@ -52,124 +52,203 @@ $(document).ready(function () {
                 break;
         }
     });
-    
+
     function getRandomInt(min, max) {
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min)) + min;
     }
-    
-    
+
+
     Ptypo.createFont('antique-font', 'antique');
     Ptypo.createFont('antique-font-game', 'antique');
-    
+
     // $('.contenteditable').contentEditable().change(function(){
     //   console.log('coucou');
     //     $('.main').text($('span.contenteditable').text);
     // });
-    
-    
+
+
     $('.main').on('focus', function() {
       before = $(this).html();
-    }).on('blur keyup paste', function() { 
+    }).on('blur keyup paste', function() {
       if (before != $(this).html()) { $(this).trigger('change'); }
     });
 
     $('.main').on('change', function() {
-       $('.outline').text($(this).text());
+       $('.outline').html($(this).html());
     });
-    
+
     let gamePositions = [
       {
         'width': 1.22,
         'xHeight': 1059,
         'ascender': 204,
         'descender': -110,
-        'thickness': 77        
+        'thickness': 77
       },
       {
         'width': 4.45,
         'xHeight': 372,
         'ascender': 275,
         'descender': -108,
-        'thickness': 77        
+        'thickness': 77
       },
       {
         'width': 3.33,
         'xHeight': 839,
         'ascender': 160,
         'descender': -201,
-        'thickness': 77        
+        'thickness': 77
       },
       {
         'width': 2.40,
         'xHeight': 660,
         'ascender': 380,
         'descender': -4,
-        'thickness': 77        
+        'thickness': 77
       },
       {
         'width': 0.14,
         'xHeight': 492,
         'ascender': 94,
         'descender': -454,
-        'thickness': 77        
+        'thickness': 77
       },
       {
         'width': 1,
         'xHeight': 391,
         'ascender': 157,
         'descender': -152,
-        'thickness': 84        
+        'thickness': 84
       },
       {
         'width': 2.58,
         'xHeight': 673,
         'ascender': 176,
         'descender': -99,
-        'thickness': 83        
+        'thickness': 83
       },
       {
         'width': 0.44,
         'xHeight': 361,
         'ascender': 128,
         'descender': -118,
-        'thickness': 83        
+        'thickness': 83
       },
       {
         'width': 2.37,
         'xHeight': 383,
         'ascender': 380,
         'descender': -267,
-        'thickness': 77        
+        'thickness': 77
       },
     ];
-    
-    let progressBar = document.getElementsByClassName("progress-bar")[0];
 
-    $('#startgame').on('click', async() => {
-      let index = 0;
-      $('.outline').show();
-      while (index < gamePositions.length) {
-        try {
-          await play(index);
-          console.log('game won!');
-        } catch (e) {
-          console.log('game lost!');
-        } finally {
-            progressBar.classList.remove("loading");
-            void progressBar.offsetWidth;
-        }
-        index++;
-        if (index >= gamePositions.length) {
-          console.log('game finished');
-          $('.outline').hide();
-          break;
+    let progressBar = document.getElementsByClassName("progress-bar")[0];
+    let background = document.getElementById("glyph");
+
+    let stopgame = false;
+    let index = 0;
+    $('#startgame').on('click', () => {
+      $('#startgame').hide();
+      $('#stopgame').show();
+      index = 0;
+      stopgame = false;
+      let score = 0;
+      let text = $('.main').html();
+
+      async function startgame() {
+        while (index < gamePositions.length) {
+          try {
+            await play(index);
+            console.log('game won!');
+            background.classList.add("success");
+            setTimeout(function () {
+              void background.offsetWidth;
+              background.classList.remove("success");
+            }, 300);
+            score ++;
+            $('#score').text('Score: ' + score);
+          } catch (e) {
+            console.log('game lost!');
+            background.classList.add("error");
+            setTimeout(function () {
+              void background.offsetWidth;
+              background.classList.remove("error");
+            }, 300);
+          } finally {
+              progressBar.classList.remove("loading");
+              void progressBar.offsetWidth;
+          }
+          index++;
+          if (index >= gamePositions.length && index !== 100) {
+            console.log('game finished');
+            Ptypo.createFont('antique-font-game', 'antique');
+            $('.outline').hide();
+            $('#startgame').show();
+            $('#stopgame').hide();
+            if (score >= 7) {
+              $('.main').html('You won! Congrats!');
+            } else {
+              $('.main').html('Game over');
+            }
+            setTimeout(function () {
+              $('.outline').show();
+              $('#score').hide();
+              $('.main').html('les puces <br> typo');
+              $('.outline').html('les puces <br> typo');
+            }, 5000);
+            break;
+          } else if (index === 100) {
+            $('.outline').hide();
+            Ptypo.createFont('antique-font-game', 'antique');
+            $('.main').text('Thanks for playing!');
+            setTimeout(function () {
+              $('.outline').show();
+              $('#score').hide();
+              $('.main').html('les puces <br> typo');
+              $('.outline').html('les puces <br> typo');
+            }, 5000);
+            break;
+          }
         }
       }
+
+      setTimeout(function () {
+        $('.main').html('Get ready...');
+        $('.outline').html('Get ready...');
+        setTimeout(function () {
+          $('.main').html('3');
+          $('.outline').html('3');
+          setTimeout(function () {
+            $('.main').html('2');
+            $('.outline').html('2');
+            setTimeout(function () {
+              $('.main').html('1');
+              $('.outline').html('1');
+              setTimeout(function () {
+                $('.outline').show();
+                $('#score').show();
+                $('.main').html(text);
+                $('.outline').html(text);
+                startgame();
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+
+
     });
-    
-    
+
+    $('#stopgame').on('click', () => {
+      $('#stopgame').hide();
+      $('#startgame').show();
+      stopgame = true;
+    });
+
+
     let play = (gameindex) => {
       return new Promise((resolve, reject) => {
         // Setup parameters
@@ -178,22 +257,26 @@ $(document).ready(function () {
         Ptypo.changeParam(gamePositions[gameindex].ascender, 'ascender', 'antique-font-game');
         Ptypo.changeParam(gamePositions[gameindex].descender, 'descender', 'antique-font-game');
         Ptypo.changeParam(gamePositions[gameindex].thickness, 'thickness', 'antique-font-game');
-        
+
         progressBar.classList.add("loading");
-        
-        //Timeout if not resolved        
+
+        //Timeout if not resolved
         setTimeout(function () {
           reject(false);
         }, 30000);
         // Check if position correct
         setInterval(function () {
+          if (stopgame) {
+            index = 99;
+            reject(false);
+          }
           if (checkIfCorrect(gameindex)) {
             resolve(true);
-          }  
+          }
         }, 50);
       });
     };
-    
+
     let getParams = () => {
       return {
         'width' : Ptypo.getParam('width', 'antique-font'),
@@ -203,21 +286,21 @@ $(document).ready(function () {
         'thickness': Ptypo.getParam('thickness', 'antique-font')
       };
     }
-    
+
     let checkIfCorrect = (gameindex) => {
       let gameparam = gamePositions[gameindex];
       let currentparam = getParams();
       if (
-        Math.abs(gameparam.width - currentparam.width) < 0.25 &&
-        Math.abs(gameparam.xHeight - currentparam.xHeight) < 40 &&
-        Math.abs(gameparam.ascender - currentparam.ascender) < 40 &&
-        Math.abs(gameparam.descender - currentparam.descender) < 40 &&
-        Math.abs(gameparam.thickness - currentparam.thickness) < 5
+        Math.abs(gameparam.width - currentparam.width) < 0.55 &&
+        Math.abs(gameparam.xHeight - currentparam.xHeight) < 60 &&
+        Math.abs(gameparam.ascender - currentparam.ascender) < 60 &&
+        Math.abs(gameparam.descender - currentparam.descender) < 60 &&
+        Math.abs(gameparam.thickness - currentparam.thickness) < 30
       ) {
         return true;
       } else return false;
     };
-    
+
     // Game logics
     var GAME_STATUS = 'waiting';
         loggedPlayers = 0;
@@ -231,11 +314,11 @@ $(document).ready(function () {
     });
 
     //KinectGestures.on(KinectGestures.EventType.PlayerTracked, function(event){});
-    
-    //KinectGestures.on(KinectGestures.EventType.PlayerEngagedAgain, function(event){});    
+
+    //KinectGestures.on(KinectGestures.EventType.PlayerEngagedAgain, function(event){});
 
     //KinectGestures.on(KinectGestures.GestureType.Wave, function(event){});
-    
+
     //KinectGestures.on(KinectGestures.GestureType.Swipe, function(event){});
 
     //KinectGestures.on('squat',function(event){});
@@ -245,18 +328,11 @@ $(document).ready(function () {
     //KinectGestures.on(KinectGestures.GestureType.Jump, function(event){});
     //KinectGestures.on(KinectGestures.GestureType.JumpPosition, function(event){});
     //KinectGestures.on(KinectGestures.GestureType.JumpPositionHip, function(event){});
-    
+
     KinectGestures.on(KinectGestures.EventType.PlayerLost, function(event){
-        $('#glyph').removeClass('stroboscopic');
-        Ptypo.changeParam(parseFloat(115), 'thickness', 'antique-font');
-        Ptypo.changeParam(parseFloat(0.6), 'curviness', 'antique-font');
-        Ptypo.changeParam(parseFloat(1), 'width', 'antique-font');
-        Ptypo.changeParam(parseFloat(520), 'xHeight', 'antique-font');
-        Ptypo.changeParam(parseFloat(230), 'ascender', 'antique-font');
-        Ptypo.changeParam(parseFloat(-230), 'descender', 'antique-font');
+        Ptypo.createFont('antique-font', 'antique');
     });
 
     //KinectGestures.on(KinectGestures.GestureType.PlayerPosition, function(event){});
 
 });
-
